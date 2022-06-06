e15705e706b43396a04158f189cbffe03f58b3e0
Paulo Marinho <tommymolto@gmail.com>
2022-06-05 21:42:58 -0300

    01-login por email ja existente

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kweedo/page/home_page.dart';
 import 'package:kweedo/widget/login_widget.dart';
 import 'animation/fade_animation.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MaterialApp(
-    home: HomePage(),
-  ));
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatelessWidget {
+  static const String title = '01- Firebase Auth';
+
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const Scaffold(body: LoginWidget(),);
+  Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: navigatorKey,
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData.dark().copyWith(
+      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.teal)
+          .copyWith(secondary: Colors.tealAccent),
+    ),
+    home: MainPage(),
+  );
 }
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Algo deu errado!'));
+        } else if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return LoginWidget();
+        }
+      },
+    ),
+  );
+}
--- lib/widget/login_widget.dart
+++ lib/widget/login_widget.dart
@@ -1,58 +1,88 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginWidget  extends StatefulWidget {
-  const LoginWidget({ Key? key }) : super(key: key);
+import '../main.dart';
 
+class LoginWidget extends StatefulWidget {
   @override
-  State<LoginWidget> createState() => _LoginWidgetState();
+  _LoginWidgetState createState() => _LoginWidgetState();
 }
 
-class _LoginWidgetState extends State<LoginWidget > {
+class _LoginWidgetState extends State<LoginWidget> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
   @override
   void dispose() {
-    super.dispose();
     emailController.dispose();
     passwordController.dispose();
+
+    super.dispose();
   }
+
   @override
   Widget build(BuildContext context) => SingleChildScrollView(
-    padding: EdgeInsets.all(16),
+    padding: const EdgeInsets.all(16),
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const SizedBox(height: 40,),
+        const SizedBox(height: 60),
+        const FlutterLogo(size: 120),
+        const SizedBox(height: 20),
+        const Text(
+          'Olá,\n Bem vindo de volta',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 40),
         TextField(
           controller: emailController,
           cursorColor: Colors.white,
           textInputAction: TextInputAction.next,
-          decoration: const InputDecoration(
-            labelText: 'Email',
-
-          ),
+          decoration: const InputDecoration(labelText: 'Email'),
         ),
-        const SizedBox(height: 4,),
+        const SizedBox(height: 4),
         TextField(
           controller: passwordController,
-          cursorColor: Colors.white,
           textInputAction: TextInputAction.done,
-          decoration: const InputDecoration(
-            labelText: 'Password',
-
-          ),
+          decoration: const InputDecoration(labelText: 'Senha'),
           obscureText: true,
         ),
-        const SizedBox(height: 20,),
+        const SizedBox(height: 20),
         ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size.fromHeight(50)
-            ),
-
-            onPressed: (){}, icon: Icon(Icons.lock_open, size: 32,), label: Text('Entrar', style: TextStyle(fontSize: 24),))
-
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(50),
+          ),
+          icon: const Icon(Icons.lock_open, size: 32),
+          label: const Text(
+            'Sign In',
+            style: TextStyle(fontSize: 24),
+          ),
+          onPressed: signIn,
+        ),
       ],
     ),
   );
+
+  Future signIn() async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
+    );
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    // Navigator.of(context) not working!
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
+  }
 }
